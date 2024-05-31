68c88f4f513f341c8376d276402d475c1254a818
SandiyaA02 <sandiya@appoly.co.uk>
2024-05-31 16:41:35 +0100

    Initial Commit

---
+++ FlickGallery/FlickGalleryApp.swift
@@ -0,0 +1,17 @@
+//
+//  FlickGalleryApp.swift
+//  FlickGallery
+//
+//  Created by Sandhya on 31/05/2024.
+//
+
+import SwiftUI
+
+@main
+struct FlickGalleryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
